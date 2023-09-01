6ac1e5583ac041033dccf1d32930ba57598d8e0e
jingoochoi <jingoo0206@gmail.com>
2023-09-01 16:57:34 +0900

    .

---
+++ img.js
@@ -0,0 +1,74 @@
+const html={
+    "egg":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/egg.jpg" alt="egg"></td>
+                    <td class="t"><h3>GOOD EGG<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td class="t"><button>pay</button></td>
+                </tr>
+            </table>`,
+    "meat":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/meat.jpg" alt="meat"></td>
+                    <td><h3>FRESH MEAT<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td><button>pay</button></td>
+                </tr>
+            </table>`,
+    "sauce":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/sauce.jpg" alt="sauce"></td>
+                    <td><h3>GOOD SAUCE<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td><button>pay</button></td>
+                </tr>
+            </table>`,
+    "vegetable":`<table align="center">
+                    <tr>
+                        <td rowspan="2"><img src="./img/vegetable.jpg"  alt="vegetable"></td>
+                        <td class="t"><h3>GOOD VEGETABLE<br>11,000won</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="t"><button>pay</button></td>
+                    </tr>
+                </table>`,
+    "bowl":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/bowl.jpg" alt="bowl"></td>
+                    <td class="t"><h3>BEAUTIFUL BOWL<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td class="t"><button>pay</button></td>
+                </tr>
+            </table>`,
+    "knife":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/knife.jpg" alt="knife"></td>
+                    <td class="t"><h3>VERY SHARP KNIVES<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td class="t"><button>pay</button></td>
+                </tr>
+            </table>`,
+    "plate":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/plate.jpg" alt="plate"></td>
+                    <td class="t"><h3>BEAUTIFUL PLATE<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td class="t"><button>pay</button></td>
+                </tr>
+            </table>`,
+    "spoonset":`<table align="center">
+                <tr>
+                    <td rowspan="2"><img src="./img/spoonset.jpg" alt="vegetable"></td>
+                    <td class="t"><h3>GOOD SPOON SET<br>11,000won</h3></td>
+                </tr>
+                <tr>
+                    <td class="t"><button>pay</button></td>
+                </tr>
+            </table>`,
+}
