6c6530548bdc3a2a1137b5289495f75d4a1153d0
jingoochoi <jingoo0206@gmail.com>
2023-09-11 16:45:10 +0900

    .

--- notice.js
+++ notice.js
@@ -1,5 +1,6 @@
 // notice js
-let number;
-let title;
-let date;
-notice=[number,title,date];
+const data=[
+    {number:'1',title:'notice1-recent',date:'2023.10.13',content:`we now announce that we will have a vacation until tomorrow.<br>so the delivery will be delayed 2 or 3days.<br>we are very sorry. thank you`},
+    {number:'2',title:'notice2-origin',date:'2023.10.10',content:`sorry for the last issue<br>we now fix it and re-check about it<br>thank you`},
+    {number:'3',title:'notice3-opening',date:'2022.12.12',content:`welcome everybody<br>we now open the shop<br>thank you`},
+]
